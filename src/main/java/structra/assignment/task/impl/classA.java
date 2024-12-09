4dcccd689e10b77ba37666a809e1c2e7d712fd1d
plavsen <166814751+plavsen@users.noreply.github.com>
2024-12-09 15:56:16 +0100

    1

---
+++ src/main/java/structra/assignment/task/impl/classA.java
@@ -0,0 +1,12 @@
+package structra.assignment.task.impl;
+
+import structra.assignment.framework.llm.KeyProvider;
+import structra.assignment.framework.llm.model.Mimic;
+
+public class classA implements KeyProvider {
+
+    @Override
+    public String getApiKey() {
+        return "";
+    }
+}
